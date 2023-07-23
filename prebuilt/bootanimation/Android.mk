cb522dc744d30f26f305b7738e73e0ced4587b82
elpaablo <donopwhile1@gmail.com>
2023-07-23 07:22:40 +0200

    Update bootanimation

--- prebuilt/bootanimation/Android.mk
+++ prebuilt/bootanimation/Android.mk
@@ -32,10 +32,10 @@ $(TARGET_GENERATED_BOOTANIMATION): $(SOONG_ZIP)
 	RESOLUTION="$$IMAGESCALEWIDTH"x"$$IMAGESCALEHEIGHT"; \
 	if [ "$$IMAGESCALEWIDTH" -eq 720 ]; then \
 	    tar xfp vendor/addons/prebuilt/bootanimation/bootanimation_720.tar -C $(INTERMEDIATES); \
-	    echo "580 580 $(BOOTFPS)" > $(INTERMEDIATES)/desc.txt; \
+	    echo "800 800 $(BOOTFPS)" > $(INTERMEDIATES)/desc.txt; \
 	else \
 	    tar xfp vendor/addons/prebuilt/bootanimation/bootanimation_1080.tar -C $(INTERMEDIATES); \
-	    echo "860 860 $(BOOTFPS)" > $(INTERMEDIATES)/desc.txt; \
+	    echo "1200 1200 $(BOOTFPS)" > $(INTERMEDIATES)/desc.txt; \
 	fi; \
 	cat vendor/addons/prebuilt/bootanimation/desc.txt >> $(INTERMEDIATES)/desc.txt;
 	$(hide) $(SOONG_ZIP) -L 0 -o $(TARGET_GENERATED_BOOTANIMATION) -C $(INTERMEDIATES) -D $(INTERMEDIATES)
