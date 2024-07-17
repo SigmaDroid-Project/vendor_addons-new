8e8c91b49c2b7fe60e0f7ef2600d4a7f9e543068
elpaablo <donopwhile1@gmail.com>
2024-07-17 07:34:04 +0100

    Implement QS Split Shade (3/3)

--- config.mk
+++ config.mk
@@ -450,6 +450,11 @@ PRODUCT_PACKAGES += \
     VolumeShadedLayer \
     VolumeAOSPRevamped
 
+# Slit shade (landscape qs style)
+PRODUCT_PACKAGES += \
+    SplitShadeLayoutOverlay \
+    SplitShadeCutoutOverlay
+
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
